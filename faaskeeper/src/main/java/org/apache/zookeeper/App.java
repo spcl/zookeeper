c0aeaf7c10ccf89469afc5978169854a8005a4cf
mujtaba1747 <mujtaba1747@gmail.com>
2024-07-12 01:49:59 +0530

    Moved FaasKeeper to a separate submodule

---
+++ faaskeeper/src/main/java/org/apache/zookeeper/App.java
@@ -0,0 +1,14 @@
+package org.apache.zookeeper;
+
+/**
+ * Hello world!
+ *
+ */
+import org.apache.zookeeper.data.Stat;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- faaskeeper/src/main/java/org/apache/zookeeper/faaskeeper/model/Node.java
+++ faaskeeper/src/main/java/org/apache/zookeeper/faaskeeper/model/Node.java
@@ -96,32 +96,32 @@ public class Node {
         return this.modifiedVersion != null;
     }
 
-    public Map<String, Object> serialize() {
-        Map<String, Object> dataDict = new HashMap<>();
-        if (this.data != null) {
-            dataDict.put("data", new String(this.data));
-        }
+    // public Map<String, Object> serialize() {
+    //     Map<String, Object> dataDict = new HashMap<>();
+    //     if (this.data != null) {
+    //         dataDict.put("data", new String(this.data));
+    //     }
 
-        Map<String, Object> versionDict = new HashMap<>();
-        if (this.createdVersion != null) {
-            versionDict.put("version", Map.of("created", this.createdVersion.serialize()));
-        }
-        if (this.modifiedVersion != null) {
-            if (!versionDict.containsKey("version")) {
-                versionDict.put("version", new HashMap<>());
-            }
-            ((Map<String, Object>) versionDict.get("version")).put("modified", this.modifiedVersion.serialize());
-        }
+    //     Map<String, Object> versionDict = new HashMap<>();
+    //     if (this.createdVersion != null) {
+    //         versionDict.put("version", Map.of("created", this.createdVersion.serialize()));
+    //     }
+    //     if (this.modifiedVersion != null) {
+    //         if (!versionDict.containsKey("version")) {
+    //             versionDict.put("version", new HashMap<>());
+    //         }
+    //         ((Map<String, Object>) versionDict.get("version")).put("modified", this.modifiedVersion.serialize());
+    //     }
 
-        Map<String, Object> childrenDict = new HashMap<>();
-        if (this.children != null) {
-            childrenDict.put("children", this.children);
-        }
-        Map<String, Object> result = new HashMap<>();
-        result.put("path", this.path);
-        result.putAll(dataDict);
-        result.putAll(versionDict);
-        result.putAll(childrenDict);
-        return result;
-    }
+    //     Map<String, Object> childrenDict = new HashMap<>();
+    //     if (this.children != null) {
+    //         childrenDict.put("children", this.children);
+    //     }
+    //     Map<String, Object> result = new HashMap<>();
+    //     result.put("path", this.path);
+    //     result.putAll(dataDict);
+    //     result.putAll(versionDict);
+    //     result.putAll(childrenDict);
+    //     return result;
+    // }
 }
